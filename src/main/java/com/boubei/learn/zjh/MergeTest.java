7dec09f79db8dc75db3b46b0abc317d9c5cc0377
Artlife-Z <820694846@qq.com>
2017-07-05 20:49:35 +0800

    merge test zjh

---
+++ src/main/java/com/boubei/learn/zjh/MergeTest.java
@@ -0,0 +1,29 @@
+package com.boubei.learn.zjh;
+
+import java.sql.Timestamp;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+import com.boubei.tss.dm.data.sqlquery.SQLExcutor;
+import com.boubei.tss.dm.data.sqlquery.SqlConfig;
+import com.boubei.tss.util.DateUtil;
+
+public class MergeTest {
+	
+	@Test
+	public void Test(){
+		String sql = SqlConfig.getScript("querySite", 4);
+		
+//		Timestamp day = new Timestamp(DateUtil.today().getTime());
+
+		Map<Integer, Object> params = new HashMap<Integer, Object>();
+		
+		params.put( 1 , "2017-7-4" );
+		
+		//params.put( 1 , "10488016781001" );
+		
+		SQLExcutor.excute(sql, params,"ds-xjy");
+	}
+}
