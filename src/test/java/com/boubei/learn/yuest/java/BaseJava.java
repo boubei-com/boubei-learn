8f5c0ca4bbee9889f95211cca088c8914e5cd284
MaYue <358540128@qq.com>
2017-12-19 17:47:58 +0800

    修改的JDBC连接基础代码

---
+++ src/test/java/com/boubei/learn/yuest/java/BaseJava.java
@@ -0,0 +1,13 @@
+package com.boubei.learn.yuest.java;
+
+public class BaseJava {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/com/boubei/learn/yuest/pgsqldb/optimizing/JdbcUtils.java
@@ -0,0 +1,13 @@
+package com.boubei.learn.yuest.pgsqldb.optimizing;
+
+public class JdbcUtils {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		private
+	}
+
+}
