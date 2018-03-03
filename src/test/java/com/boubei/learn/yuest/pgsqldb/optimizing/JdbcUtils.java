ae0a1faa13fd82abfcb6e05dee727fb82163776c
Jon.King <jinpujun@gmail.com>
2018-03-03 11:06:37 +0800

    fix

--- src/test/java/com/boubei/learn/yuest/pgsqldb/optimizing/JdbcUtils.java
+++ src/test/java/com/boubei/learn/yuest/pgsqldb/optimizing/JdbcUtils.java
@@ -1,10 +1,5 @@
 package com.boubei.learn.yuest.pgsqldb.optimizing;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 
 public final class JdbcUtils {
 	//??
--- src/test/java/com/boubei/learn/yuest/yuestTest.java
+++ src/test/java/com/boubei/learn/yuest/yuestTest.java
@@ -1,6 +1,6 @@
 package com.boubei.learn.yuest;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
